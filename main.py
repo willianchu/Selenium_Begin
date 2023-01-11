f3dad11944548982051b75b1ac606d9ec079e6f5
willianchu <willianchu@hotmail.com>
2023-01-11 13:43:05 -0800

    Clicking in lighthouse buttons courses by title

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 # find_element_by_id(‘id’) /	find_element(By.ID, ‘id’)
 # find_element_by_name(‘name’)	/ find_element(By.NAME, ‘name’)
 # find_element_by_xpath(‘xpath’)	/ find_element(By.XPATH, ‘xpath’)
+
+# Syntax WebElement elm = driver. findElement (By.className ("input__input")); WebElement p = driver.findElement (By.xpath ("//input [@class = ' input__input']")); WebElement t = driver. findElement (By.cssSelector ("input [class=' input__input']"));
+
 import os
 import time
 from selenium import webdriver
@@ -11,10 +14,10 @@ from selenium.webdriver.common.by import By
 os.environ['PATH'] += ':/usr/local/bin'
 driver = webdriver.Chrome()
 
-driver.get("https://www.dofactory.com/html/button/id#:~:text=The%20id%20attribute%20assigns%20an%20identifier%20to%20the,to%20any%20HTML%20element.%20A%20unique%20alphanumeric%20string.")
+driver.get("https://www.lighthouselabs.ca/")
 driver.implicitly_wait(30)
-myElement = driver.find_element(By.ID, "alert-button")
+myElement = driver.find_element(By.XPATH, "//a[@title='Courses']")
 myElement.click()
-myElement = driver.find_element(By.ID, "random")
+
 time.sleep(5)
 
