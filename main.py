5194e73e2d6e65a61fbe5a1765ca1cb047f02e6d
willianchu <willianchu@hotmail.com>
2023-01-11 12:57:27 -0800

    implicity wait until 30 or object load

--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 import os
 from selenium import webdriver
 
+
 os.environ['PATH'] += ':/usr/local/bin'
 driver = webdriver.Chrome()
 
-driver.get("https://www.lighthouselabs.ca/en")
-myElement = driver.findElement(By.className("btn apply-btn"))
+driver.get("https://www.dofactory.com/html/button/id#:~:text=The%20id%20attribute%20assigns%20an%20identifier%20to%20the,to%20any%20HTML%20element.%20A%20unique%20alphanumeric%20string.")
+driver.implicitly_wait(30)
+myElement = driver.find_element_by_id("alert-button")
 myElement.click()
 
 # wait 10 seconds for the page to load
 
-driver.implicitly_wait(20)
