250306e229b38ca7bd689fd1c08cef53bbf95154
willianchu <willianchu@hotmail.com>
2023-01-11 13:09:57 -0800

    find element new syntax

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-
+# Old API /	New API
+# find_element_by_id(‘id’) /	find_element(By.ID, ‘id’)
+# find_element_by_name(‘name’)	/ find_element(By.NAME, ‘name’)
+# find_element_by_xpath(‘xpath’)	/ find_element(By.XPATH, ‘xpath’)
 import os
+import time
 from selenium import webdriver
-
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 os.environ['PATH'] += ':/usr/local/bin'
 driver = webdriver.Chrome()
 
 driver.get("https://www.dofactory.com/html/button/id#:~:text=The%20id%20attribute%20assigns%20an%20identifier%20to%20the,to%20any%20HTML%20element.%20A%20unique%20alphanumeric%20string.")
 driver.implicitly_wait(30)
-myElement = driver.find_element_by_id("alert-button")
+myElement = driver.find_element(By.ID, "alert-button")
 myElement.click()
-
-# wait 10 seconds for the page to load
+time.sleep(5)
 
