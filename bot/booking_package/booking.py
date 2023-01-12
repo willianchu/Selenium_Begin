80f505ad61b1f9eaf9e559ebc1d07b43dc791ea9
willianchu <willianchu@hotmail.com>
2023-01-12 11:18:04 -0800

    implicit wait and maximize browser

--- bot/booking_package/booking.py
+++ bot/booking_package/booking.py
@@ -9,6 +9,8 @@ class Booking(webdriver.Chrome):
     self.teardown = teardown
     os.environ['PATH'] += self.driver_path # add the driver to the PATH or else Selenium will complain
     super(Booking, self).__init__() # this is the same as super().__init__()
+    self.implicitly_wait(15) # wait for 10 seconds 
+    self.maximize_window()
   
   def __exit__(self, exc_type, exc_value, traceback): # default parameters
     if self.teardown:
