47b38d31f2247265f330f1734b097cdce7cf645f
willianchu <willianchu@hotmail.com>
2023-01-12 10:07:32 -0800

    improving using AS

--- bot/booking_package/booking.py
+++ bot/booking_package/booking.py
@@ -4,10 +4,15 @@ from selenium import webdriver
 
 
 class Booking(webdriver.Chrome):
-  def __init__(self, driver_path=const.DRIVER_PATH):
+  def __init__(self, driver_path=const.DRIVER_PATH, teardown=False):
     self.driver_path = driver_path 
+    self.teardown = teardown
     os.environ['PATH'] += self.driver_path # add the driver to the PATH or else Selenium will complain
     super(Booking, self).__init__() # this is the same as super().__init__()
+  
+  def __exit__(self, exc_type, exc_value, traceback): # default parameters
+    if self.teardown:
+      self.quit()
 
   def land_first_page(self):
     self.get(const.BASE_URL)
--- bot/start_bot.py
+++ bot/start_bot.py
@@ -1,4 +1,9 @@
 from booking_package.booking import Booking
 
-inst = Booking()
-inst.land_first_page()
+# Version 1.0
+# inst = Booking()
+# inst.land_first_page()
+
+with Booking(teardown=True) as bot:
+  bot.land_first_page()
+  print('Closing bot...')
