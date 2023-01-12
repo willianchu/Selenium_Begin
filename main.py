32ba28076ccbd6e77c58d23f1841337046d18f73
willianchu <willianchu@hotmail.com>
2023-01-11 18:02:59 -0800

    insert text in form fields

--- main.py
+++ main.py
@@ -17,11 +17,20 @@ from selenium.webdriver.support import expected_conditions as EC
 os.environ['PATH'] += ':/usr/local/bin'
 driver = webdriver.Chrome()
 
-driver.get("https://www.lighthouselabs.ca/")
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//a[@title='Courses']")))
-# EC.text_to_be_present_in_element((By.XPATH, "//a[@title='Courses']"), "Courses")
-myElement = driver.find_element(By.XPATH, "//a[@title='Courses']")
-myElement.click()
-
+driver.get("https://www.seleniumeasy.com/python/locating-elements-in-selenium-python")
+nameID = driver.find_element(By.ID, "edit-name")
+nameID.send_keys("Joel Campbell")
+commentID = driver.find_element(By.ID, "edit-comment-body-und-0-value")
+commentID.send_keys("This is a comment super cool made by Joel Campbell Bot")
 time.sleep(5)
+
+
+
+
+# driver.get("lighthouselabs.ca")
+# WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//a[@title='Courses']")))
+# # EC.text_to_be_present_in_element((By.XPATH, "//a[@title='Courses']"), "Courses")
+# myElement = driver.find_element(By.XPATH, "//a[@title='Courses']")
+# myElement.click()
+# time.sleep(5)
 
